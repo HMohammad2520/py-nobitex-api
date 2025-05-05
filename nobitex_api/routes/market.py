4d06f99cf0819349a6d1bed59f14967c93eb632b
Mohammad Heydari <HMohammad2520@Gmail.com>
2025-05-06 03:24:50 +0330

    Fixed Issue

--- nobitex_api/routes/market.py
+++ nobitex_api/routes/market.py
@@ -26,52 +26,52 @@ class Market(NobitexRoute):
         """
         Add a new order.
         """
-        ...
+        raise NotImplementedError
 
     def get_order_status(self) -> dict:
         """
         Get the status of an order.
         """
-        ...
+        raise NotImplementedError
 
     def get_orders_list(self) -> dict:
         """
         Get the list of orders.
         """
-        ...
+        raise NotImplementedError
 
     def update_order_status(self) -> dict:
         """
         Update the status of an order.
         """
-        ...
+        raise NotImplementedError
 
     def cancel_old_orders(self) -> dict:
         """
         Cancel old orders.
         """
-        ...
+        raise NotImplementedError
 
     def get_trades_list(self) -> dict:
         """
         Get the list of trades.
         """
-        ...
+        raise NotImplementedError
 
     def get_stats(self) -> dict:
         """
         Get market stats.
         """
-        ...
+        raise NotImplementedError
 
     def get_udf_history(self) -> dict:
         """
         Get UDF history.
         """
-        ...
+        raise NotImplementedError
 
     def get_global_stats(self) -> dict:
         """
         Get global market stats.
         """
-        ...
+        raise NotImplementedError
