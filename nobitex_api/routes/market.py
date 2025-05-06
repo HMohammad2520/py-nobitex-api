e304710cbccef16b027de5b7a0dcc80e3ecad439
Mohammad Heydari <HMohammad2520@Gmail.com>
2025-05-06 12:36:32 +0330

    Added Logic

--- nobitex_api/routes/market.py
+++ nobitex_api/routes/market.py
@@ -1,17 +1,18 @@
-from typing import Optional
+from typing import Literal, Optional
+from nobitex_api.currency import CurrencyEnum
 from nobitex_api.type_hints import NobitexBool, NobitexCaptcha
 from ._base import NobitexRoute
 
 """
-https://api.nobitex.ir/market/orders/add 
-https://api.nobitex.ir/market/orders/status 
-https://api.nobitex.ir/market/orders/list 
-https://api.nobitex.ir/market/orders/update-status 
-https://api.nobitex.ir/market/orders/cancel-old 
-https://api.nobitex.ir/market/trades/list 
-https://api.nobitex.ir/market/stats 
-https://api.nobitex.ir/market/udf/history 
-https://api.nobitex.ir/market/global-stats 
+https://api.nobitex.ir/market/orders/add #Done
+https://api.nobitex.ir/market/orders/status #Done
+https://api.nobitex.ir/market/orders/list #Done
+https://api.nobitex.ir/market/orders/update-status #Done
+https://api.nobitex.ir/market/orders/cancel-old #Done
+https://api.nobitex.ir/market/trades/list #Done
+https://api.nobitex.ir/market/stats #Done
+https://api.nobitex.ir/market/udf/history #Done
+https://api.nobitex.ir/market/global-stats #Done
 """
 
 class Market(NobitexRoute):
@@ -22,56 +23,303 @@ class Market(NobitexRoute):
     _route_path: str = 'market'
     _version: str = ''
 
-    def add_order(self) -> dict:
+    def add_order(
+            self,
+            type: Literal['buy','sell'],
+            src_currency: CurrencyEnum,
+            dst_currency: CurrencyEnum,
+            amount: float,
+            price: float,
+            mode: Optional[Literal['oco']] = None,
+            client_order_id: Optional[str] = None,
+            stop_price: Optional[float] = None,
+            stopLimitPrice: Optional[float] = None,
+            execution: Literal['market', 'limit', 'stop_market', 'stop_limit'] = 'limit',
+        ) -> dict:
         """
         Add a new order.
-        """
-        raise NotImplementedError
 
-    def get_order_status(self) -> dict:
+        Args:
+            type: Type of order. Can be 'buy' or 'sell'.
+            src_currency: Source currency of the order.
+            dst_currency: Destination currency of the order.
+            amount: Amount of the order.
+            price: Price of the order.
+            mode: Optional. Mode of the order. Can be 'oco'.
+            client_order_id: Optional. Client order id.
+            stop_price: Optional. Stop price of the order.
+            stopLimitPrice: Optional. Stop limit price of the order.
+            execution: Optional. Execution type of the order. Can be 'market', 'limit', 'stop_market', 'stop_limit'.
+
+        Returns:
+            dict: Response of the API.
+        """
+        post_parameters = {
+            'type': type,
+            'srcCurrency': src_currency.value,
+            'dstCurrency': dst_currency.value,
+            'amount': amount,
+            'price': price,
+            'mode': mode,
+            'clientOrderId': client_order_id,
+            'stopPrice': stop_price,
+            'stopLimitPrice': stopLimitPrice,
+            'execution': execution,
+        }
+        post_parameters = {k: v for k, v in post_parameters.items() if v is not None}
+
+        return self._client._send_request(
+            request_method = 'POST', 
+            route = self._create_route('orders', 'add'),
+            post_parms = post_parameters,
+        )
+
+    def get_order_status(
+            self,
+            id: int,
+            client_order_id: Optional[str],
+        ) -> dict:
         """
         Get the status of an order.
-        """
-        raise NotImplementedError
 
-    def get_orders_list(self) -> dict:
+        Args:
+            id: Id of the order.
+            client_order_id: Optional. Client order id.
+
+        Returns:
+            dict: Response of the API.
+        """
+        post_params = {
+            'id': id,
+            'clientOrderId': client_order_id,
+        }
+        post_params = {k: v for k, v in post_params.items() if v is not None}
+
+        return self._client._send_request(
+            request_method = 'POST',
+            route = self._create_route('orders', 'status'),
+            post_parms = post_params,
+        )
+
+    def get_orders_list(
+            self,
+            type: Literal['buy', 'sell'],
+            trade_type: Literal['spot', 'margin'],
+            src_currency: CurrencyEnum,
+            dst_currency: CurrencyEnum,
+            details: Literal[1, 2] = 1,
+            from_id: int = 1,
+            order: Optional[Literal['id', 'created_at', 'price']] = None,
+            execution: Literal['market', 'limit', 'stop_market', 'stop_limit'] = 'limit',
+            status: Literal['all', 'open', 'done', 'close'] = 'open',
+        ) -> dict:
         """
         Get the list of orders.
-        """
-        raise NotImplementedError
 
-    def update_order_status(self) -> dict:
+        Args:
+            type: Type of the order.
+            trade_type: Type of the trade.
+            src_currency: Source currency.
+            dst_currency: Destination currency.
+            details: Details level of the order.
+            from_id: Fetch reults after this ID.
+            order: Order of the fetched result.
+            execution: Execution type of the order.
+            status: Filter by status of the order.
+            
+        Returns:
+            dict: Response of the API.
+        """
+        post_params = {
+            'type': type,
+            'tradeType': trade_type,
+            'srcCurrency': src_currency.value,
+            'dstCurrency': dst_currency.value,
+            'details': details,
+            'fromId': from_id,
+            'order': order,
+            'execution': execution,
+            'status': status,
+        }
+        post_params = {k: v for k, v in post_params.items() if v is not None}
+
+        return self._client._send_request(
+            request_method = 'GET',
+            route = self._create_route('orders', 'list'),
+            post_parms = post_params,
+        )
+
+    def update_order_status(
+            self,
+            status: Literal['active', 'canceled'],
+            order_id: int = None,
+            clint_order_id: str = None,
+        ) -> dict:
         """
         Update the status of an order.
-        """
-        raise NotImplementedError
 
-    def cancel_old_orders(self) -> dict:
+        Args:
+        status: Status of the order.
+        order_id: Order id.
+        clint_order_id: Client order id.
+        """
+        
+        post_params = {
+            'status': status,
+            'order': order_id,
+            'clientOrderId': clint_order_id
+        }
+        post_params = {k: v for k, v in post_params.items() if v is not None}
+
+        return self._client._send_request(
+            request_method = 'POST',
+            route = self._create_route('orders', 'update-status'),
+            post_parms = post_params,
+        )
+
+    def cancel_old_orders(
+            self,
+            hours: float,
+            execution: Optional[Literal['market', 'limit', 'stop_market', 'stop_limit']] = None,
+            trade_type: Optional[Literal['margin', 'spot']] = None,
+            src_currenct: Optional[CurrencyEnum] = None,
+            dst_currency: Optional[CurrencyEnum] = None,
+        ) -> dict:
         """
         Cancel old orders.
-        """
-        raise NotImplementedError
 
-    def get_trades_list(self) -> dict:
+        Args:
+            hours: Hours to cancel orders.
+            execution: Execution type.
+            trade_type: Trade type.
+            src_currency: Source currency.
+            dst_currency: Destination currency.
+
+        Returns:
+            dict: Response from the server.
+        """
+        
+        post_params = {
+            'hours': hours,
+            'execution': execution,
+            'tradeType': trade_type,
+            'srcCurrency': src_currenct.value,
+            'dstCurrency': dst_currency.value,
+        }
+        post_params = {k: v for k, v in post_params.items() if v is not None}
+        
+        return self._client._send_request(
+            request_method = 'POST',
+            route = self._create_route('orders', 'cancel-old'),
+            post_parms = post_params,
+        )
+
+    def get_trades_list(
+            self,
+            src_currency: Optional[CurrencyEnum] = None,
+            dst_currency: Optional[CurrencyEnum] = None,
+            from_id : Optional[int] = None,
+        ) -> dict:
         """
         Get the list of trades.
-        """
-        raise NotImplementedError
 
-    def get_stats(self) -> dict:
+        Args:
+            src_currency: Source currency.
+            dst_currency: Destination currency.
+            from_id: From id.
+
+        Returns:
+            dict: Response from the server.
+        """
+
+        get_parms = {
+            'srcCurrency': src_currency.value,
+            'dstCurrency': dst_currency.value,
+            'fromId': from_id,
+        }
+        get_parms = {k: v for k, v in get_parms.items() if v is not None}
+
+        return self._client._send_request(
+            request_method='GET',
+            route=self._create_route('trades', 'list'),
+            get_parms=get_parms,
+        )
+
+    def get_stats(
+            self,
+            src_currency: Optional[CurrencyEnum] = None,
+            dst_currency: Optional[CurrencyEnum] = None,
+        ) -> dict:
         """
         Get market stats.
-        """
-        raise NotImplementedError
 
-    def get_udf_history(self) -> dict:
+        Args:
+            src_currency: Source currency.
+            dst_currency: Destination currency.
+
+        Returns:
+            dict: Response from the server.
+        """
+
+        get_parms = {
+            'srcCurrency': src_currency.value,
+            'dstCurrency': dst_currency.value,
+        }
+        get_parms = {k: v for k, v in get_parms.items() if v is not None}
+        
+        return self._client._send_request(
+            request_method='GET',
+            route=self._create_route('stats'),
+            get_parms=get_parms,
+        )
+
+    def get_udf_history(
+            self,
+            symbol: CurrencyEnum,
+            resolution: Literal['1', '5', '15', '30','60', '180', '240', '360', '720', 'D','2D', '3D'],
+            to: int,
+            fr: int = None,
+            count_back: int = None,
+            page: int = None,
+            ) -> dict:
         """
         Get UDF history.
+
+        Args:
+            symbol: Symbol.
+            resolution: Resolution.
+            to: To timestamp.
+            fr: From timestamp.
+            count_back: Count back.
+            page: Page number.
+        
+        Returns:
+            dict: Response from the server.
         """
-        raise NotImplementedError
+        get_parms = {
+            'symbol': symbol,
+            'resolution': resolution,
+            'to': to,
+            'from': fr,
+            'countBack': count_back,
+            'page': page,
+            }
+        get_parms = {k: v for k, v in get_parms.items() if v is not None}
+
+        return self._client._send_request(
+            request_method='GET',
+            route=self._create_route('udf', 'history'),
+            get_parms=get_parms,
+        )
 
     def get_global_stats(self) -> dict:
         """
         Get global market stats.
+
+        Returns:
+            dict: Response from the server.
         """
-        raise NotImplementedError
+        return self._client._send_request(
+            request_method='POST',
+            route=self._create_route('global-stats'),
+        )
